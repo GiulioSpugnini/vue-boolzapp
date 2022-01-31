f0a953bcb5c0f7afda724899724519a49647e9cb
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-31 20:15:55 +0100

    added when click a contact, open that chat

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const app = new Vue({
     el: '#root',
     data: {
         inputText: '',
+        currentContact: {
+            name: '',
+            avatar: '',
+        },
         messages: [{
             userMessages: 'Voglio andare in piscina',
             answer: 'Come vuoi',
@@ -16,18 +20,22 @@ const app = new Vue({
         contacts: [{
                 name: 'Michele',
                 avatar: '_1.jpg',
+
             },
             {
                 name: 'Fabio',
                 avatar: '_2.jpg',
+
             },
             {
                 name: 'Samuele',
                 avatar: '_3.jpg',
+
             },
             {
                 name: 'Luisa',
                 avatar: '_4.jpg',
+
             },
         ]
     },
@@ -35,9 +43,6 @@ const app = new Vue({
         addMessage() {
             this.messages.push({ userMessages: this.inputText, answer: 'Come vuoi' });
             this.inputText = '';
-        },
-        isActive(index) {
-            if (this.contact === index) return this.contact.name;
         },
         getDate() {
             const currentDate = new Date().toLocaleDateString();
@@ -49,6 +54,9 @@ const app = new Vue({
             const seconds = new Date().getSeconds();
             const currentTime = `${hours}:${minutes}:${seconds}`;
             return currentTime;
+        },
+        selectedContact(index) {
+            return this.currentContact = { name: this.contacts[index].name, avatar: this.contacts[index].avatar };
         },
     },
 })
