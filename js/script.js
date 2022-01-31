f463197d3a01dd3829a082741e6d4d013dbf51e7
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-31 15:23:21 +0100

    user-nav implemented, changed chat bg-image

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log('OK VUE', Vue);
 const app = new Vue({
     el: '#root',
     data: {
+        images: ['img/avatar_1', 'img/avatar_2', 'img/avatar_3', 'img/avatar_4', 'img/avatar_5', 'img/avatar_6', 'img/avatar_7', 'img/avatar_8'],
         user: {
             name: 'Nome Utente',
             avatar: '_io'
